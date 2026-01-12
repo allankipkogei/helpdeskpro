0f09d89fd8c42c2feb8c2ab85da1faac1217af96
Allan Korir <allan.korir@UFAA.GO.KE>
2026-01-12 10:13:39 +0300

    Polish ticket creation form UI for portfolio/demo

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
